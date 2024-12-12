dc825fb9a3308a36490d13c24b9f48590c394440
William Whatley <53503727+RenegadeActual@users.noreply.github.com>
2024-12-12 09:23:04 -0800

    Delete Properties/build directory

--- Properties/build/goldmaster-core.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-: call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat"
-
-cd ../Build/BepInEx/plugins
-sn -Ra Renegade.Core.dll ../../../Properties/signing/private.snk
-
-pause
